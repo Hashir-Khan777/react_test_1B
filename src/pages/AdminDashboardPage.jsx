91e22b93ef044bb0b86b0f0c7a286d51a85625ab
Hashir-Khan777 <khnhashir555@gmail.com>
2022-09-29 23:53:56 +0500

    Done task Call paginate api as shown below to get video data. Show 10 per page. Have a prev button at bottom when clicked, load prev 10 videos

--- src/pages/AdminDashboardPage.jsx
+++ src/pages/AdminDashboardPage.jsx
@@ -3,9 +3,10 @@ import { useNavigate } from "react-router";
 import { AuthContext } from "../authContext";
 import MkdSDK from "../utils/MkdSDK";
 
-const page = 1;
 const AdminDashboardPage = () => {
   const [videos, setVideos] = useState(null);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
 
   const { dispatch } = React.useContext(AuthContext);
 
@@ -13,101 +14,169 @@ const AdminDashboardPage = () => {
 
   const sdk = new MkdSDK();
 
-  useEffect(() => {
-    sdk.setTable("video");
-    sdk._method = "PAGINATE";
-    sdk.callRestAPI({ page, limit: 10 }, "PAGINATE").then((data) => {
-      setVideos(data);
-    });
-  }, []);
-
   const logOut = () => {
     dispatch({ type: "LOGOUT" });
     navigate("/admin/login", { replace: true });
   };
 
+  useEffect(() => {
+    sdk.setTable("video");
+    sdk._method = "PAGINATE";
+    sdk.callRestAPI({ page, limit: 10 }, "PAGINATE").then((data) => {
+      setPage(data.page);
+      setVideos(data);
+      setTotalPages(data.num_pages);
+    });
+  }, [page]);
+
   return (
-    <div className="w-full h-screen text-white px-28 py-11 bg-zinc-900">
-      <div className="flex justify-between items-center">
-        <div>
-          <p className="text-4xl font-black">App</p>
-        </div>
-        <div>
-          <button
-            onClick={logOut}
-            className="flex justify-between items-center bg-lime-500 py-3 px-6 rounded-full"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-            >
-              <g clip-path="url(#clip0_1755_592)">
-                <path
-                  d="M5 20C5 17.544 6.991 15.553 9.447 15.553H14.553C17.009 15.553 19 17.544 19 20"
-                  stroke="#696969"
-                  stroke-width="1.4824"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-                <path
-                  d="M15.0052 5.2448C16.6649 6.90453 16.6649 9.59548 15.0052 11.2552C13.3455 12.9149 10.6545 12.9149 8.9948 11.2552C7.33507 9.59548 7.33507 6.90453 8.9948 5.2448C10.6545 3.58507 13.3455 3.58507 15.0052 5.2448"
-                  stroke="#696969"
-                  stroke-width="1.5"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </g>
-              <defs>
-                <clipPath id="clip0_1755_592">
-                  <rect width="24" height="24" fill="white" />
-                </clipPath>
-              </defs>
-            </svg>
-            <p className="text-gray-900 ml-1">Logout</p>
-          </button>
-        </div>
-      </div>
-      <div className="mt-32">
-        <div className="flex justify-between items-center">
-          <div>
-            <p className="text-3xl font-thin">Today’s leaderboard</p>
+    <>
+      {videos ? (
+        <div className="w-full h-full text-white px-28 py-11 bg-zinc-900">
+          <div className="flex justify-between items-center">
+            <div>
+              <p className="text-4xl font-black">App</p>
+            </div>
+            <div>
+              <button
+                onClick={logOut}
+                className="flex justify-between items-center bg-lime-500 py-3 px-6 rounded-full"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                >
+                  <g clip-path="url(#clip0_1755_592)">
+                    <path
+                      d="M5 20C5 17.544 6.991 15.553 9.447 15.553H14.553C17.009 15.553 19 17.544 19 20"
+                      stroke="#696969"
+                      stroke-width="1.4824"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                    <path
+                      d="M15.0052 5.2448C16.6649 6.90453 16.6649 9.59548 15.0052 11.2552C13.3455 12.9149 10.6545 12.9149 8.9948 11.2552C7.33507 9.59548 7.33507 6.90453 8.9948 5.2448C10.6545 3.58507 13.3455 3.58507 15.0052 5.2448"
+                      stroke="#696969"
+                      stroke-width="1.5"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </g>
+                  <defs>
+                    <clipPath id="clip0_1755_592">
+                      <rect width="24" height="24" fill="white" />
+                    </clipPath>
+                  </defs>
+                </svg>
+                <p className="text-gray-900 ml-1">Logout</p>
+              </button>
+            </div>
           </div>
-          <div className="flex justify-between items-center bg-zinc-800 px-6 py-4 rounded-xl">
-            <p className="text-white mr-2">30 May 2022</p>
-            <div className="w-1 h-1 rounded-full bg-zinc-400"></div>
-            <p className="bg-lime-500 text-black mx-4 px-2 py-1 rounded-lg">
-              Submissions OPEN
-            </p>
-            <div className="w-1 h-1 rounded-full bg-zinc-400"></div>
-            <p className="text-white ml-2">11:34</p>
+          <div className="mt-24">
+            <div className="flex justify-between items-center">
+              <div>
+                <p className="text-3xl font-thin">Today’s leaderboard</p>
+              </div>
+              <div className="flex justify-between items-center bg-zinc-800 px-6 py-4 rounded-xl">
+                <p className="text-white mr-2">30 May 2022</p>
+                <div className="w-1 h-1 rounded-full bg-zinc-400"></div>
+                <p className="bg-lime-500 text-black mx-4 px-2 py-1 rounded-lg">
+                  Submissions OPEN
+                </p>
+                <div className="w-1 h-1 rounded-full bg-zinc-400"></div>
+                <p className="text-white ml-2">11:34</p>
+              </div>
+            </div>
+            {/* Table */}
+            <div className="table-auto mt-9 w-full">
+              <div>
+                <div className="flex items-center px-6">
+                  <div style={{ flex: 0.1 }} className="text-left">
+                    <p className="w-20 text-zinc-700">#</p>
+                  </div>
+                  <div style={{ flex: 1 }} className="text-zinc-700 text-left">
+                    Title
+                  </div>
+                  <div style={{ flex: 0.25 }} className="text-left">
+                    <p className="w-20 text-zinc-700">Author</p>
+                  </div>
+                  <div style={{ flex: 0.25 }} className="text-left">
+                    <p className="w-20 text-zinc-700">Most Liked</p>
+                  </div>
+                </div>
+              </div>
+              <div className="w-full">
+                {videos &&
+                  videos.list.map((video) => (
+                    <div
+                      className="flex items-center border border-zinc-700 my-2 rounded-lg py-10 px-6"
+                      key={video.id}
+                    >
+                      <div style={{ flex: 0.1 }} className="text-left">
+                        <p className="w-20 text-zinc-700">
+                          {video.id < 10 ? `0${video.id}` : video.id}
+                        </p>
+                      </div>
+                      <div
+                        style={{ flex: 1.05 }}
+                        className="flex text-left items-center"
+                      >
+                        <img
+                          src="https://pixabay.com/get/g1ce3c25c364ea4eeff1f5634fbe0fc5540fb7c1c98b086db9f042e29df6c9204e3d3e3b19a9597fb47180cfe5d8a04b2_1280.jpg"
+                          className="w-20 h-full rounded-lg mr-1"
+                          alt=""
+                        />
+                        <p className="text-zinc-700">{video.title}</p>
+                      </div>
+                      <div
+                        style={{ flex: 0.25 }}
+                        className="flex items-center text-zinc-700 text-left"
+                      >
+                        <img
+                          src="https://pixabay.com/get/g1ce3c25c364ea4eeff1f5634fbe0fc5540fb7c1c98b086db9f042e29df6c9204e3d3e3b19a9597fb47180cfe5d8a04b2_1280.jpg"
+                          className="w-8 h-8 rounded-full mr-1"
+                          alt=""
+                        />
+                        <p className="w-20 text-zinc-600">{video.username}</p>
+                      </div>
+                      <div style={{ flex: 0.25 }} className="text-left">
+                        <p className="w-20 text-zinc-700">{video.like}</p>
+                      </div>
+                    </div>
+                  ))}
+              </div>
+            </div>
+            <div className="flex justify-between items-center">
+              {page > 1 ? (
+                <button
+                  onClick={() => setPage(page - 1)}
+                  className="flex justify-between items-center bg-lime-500 py-3 px-6 rounded-full"
+                >
+                  Previous
+                </button>
+              ) : (
+                <div />
+              )}
+              {page < totalPages && (
+                <button
+                  onClick={() => setPage(page + 1)}
+                  className="flex justify-between items-center bg-lime-500 py-3 px-6 rounded-full"
+                >
+                  Next
+                </button>
+              )}
+            </div>
           </div>
         </div>
-        <table className="table-auto mt-9 w-full">
-          <thead>
-            <tr>
-              <th className="text-left">#</th>
-              <th className="text-left">Title</th>
-              <th className="text-left">Author</th>
-              <th className="text-left">Most Liked</th>
-            </tr>
-          </thead>
-          <tbody>
-            {videos &&
-              videos.list.map((video) => (
-                <tr key={video.id}>
-                  <td>{video.id < 10 ? `0${video.id}` : video.id}</td>
-                  <td>{video.title}</td>
-                  <td>{video.username}</td>
-                  <td>{video.like}</td>
-                </tr>
-              ))}
-          </tbody>
-        </table>
-      </div>
-    </div>
+      ) : (
+        <div className="flex justify-center items-center w-full h-screen text-white px-28 py-11 bg-zinc-900">
+          <p className="text-xl">Loading...</p>
+        </div>
+      )}
+    </>
   );
 };
 
